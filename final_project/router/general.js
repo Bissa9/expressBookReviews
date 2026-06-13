0ac5798fb7b0b9493e7dbfe3ba93cbad6f1fc75e
Bissa9 <bissakld@gmail.com>
2026-06-13 16:55:12 -0400

    Fix

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -42,23 +42,29 @@ public_users.get('/',async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',async function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
+public_users.get('/isbn/:isbn', async function (req, res) {
 
-  try {
-    const response = await axios.get('http://localhost:5000/books');
+    const isbn = req.params.isbn;
 
-    const allBooks = response.data;
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        const allBooks = response.data;
 
-    return res.status(200).json(allBooks[isbn]);
-  } catch (error) {
-    return res.status(500).json({
-      message: "Error retrieving book"
-    });
-  }
-  
- });
+        if (!allBooks[isbn]) {
+            return res.status(404).json({
+                message: "Book not found"
+            });
+        }
+
+        return res.status(200).json(allBooks[isbn]);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error retrieving book"
+        });
+    }
+
+});
   
 // Get book details based on author
 public_users.get('/author/:author',async function (req, res) {
