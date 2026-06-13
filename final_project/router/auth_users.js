56626121da6787227634fdcc10f62868fb0232f5
Bissa9 <bissakld@gmail.com>
2026-06-13 12:37:19 -0400

    Completed Task 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,11 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    let usersWithSameName = users.filter(
+        (user) => user.username === username
+    );
+
+    return usersWithSameName.length === 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
